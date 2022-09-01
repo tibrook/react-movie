f016bcf7a2c288f95fd391c217cb9a07ff6c631f
tibrook <guillouet.alexandre35@gmail.com>
2022-09-01 04:32:32 +0200

    initial project version

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+const Header = () => {
+  return (
+    <div className="header">
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={(nav) => (nav.isActive ? "nav-active" : "")}
+            >
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/coups-de-coeur"
+              className={(nav) => (nav.isActive ? "nav-active" : "")}
+            >
+              Coups de coeur
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/LikePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+
+const LikePage = () => {
+  return (
+    <div className="like-page">
+      <Header />
+    </div>
+  );
+};
+
+export default LikePage;
